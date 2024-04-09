35e833b9e6f0fb553c2a80b07981b0f7e412ced0
Lucas Cardoso <adslucascardoso@gmail.com>
2024-04-09 20:14:16 -0300

    update

--- costs/src/components/pages/newProjects/NewProjects.js
+++ costs/src/components/pages/newProjects/NewProjects.js
@@ -1,12 +1,36 @@
+
 import Projectform from "../../projectForm/ProjectForm";
 import styles from "./NewProjects.module.css";
+import { useNavigate  } from 'react-router-dom';
 
 function NewProjects() {
+  const navigate = useNavigate();
+
+  function createPost(project) {
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        ContentType: "applicaton/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        //redirect
+        navigate("/projects", { message: "Projeto criado com sucesso" });
+      })
+      .catch((error) => console.log(error));
+  }
+
   return (
     <div className={styles.newproject_container}>
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar serviços!</p>
-      <Projectform btnText="Criar Projeto" />
+      <Projectform handleSubmit={createPost} btnText="Criar Projeto" />
     </div>
   );
 }
--- costs/src/components/projectForm/ProjectForm.js
+++ costs/src/components/projectForm/ProjectForm.js
@@ -4,44 +4,71 @@ import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 import styles from "./ProjectForm.module.css";
 
-function Projectform({ btnText }) {
+function Projectform({ handleSubmit, btnText, projectData }) {
   const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {});
 
   /* Hook 'useEffect' usado para renderizar uma unica vez por load
   O useEffect é chamado após a renderização do componente. Ele pode ser usado para realizar várias tarefas, como buscar dados, manipular o DOM diretamente, ou realizar qualquer outra operação que não seja renderização. Isso o torna ideal para operações que precisam ocorrer após a renderização do componente, como atualizações de estado assíncronas, inscrições em eventos, ou limpeza de recursos.
   */
-useEffect(() => {
-  fetch("http://localhost:5000/categories", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      setCategories(data);
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .catch((error) => console.log(error));
-},[])
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const submit = (event) => {
+    event.preventDefault();
+    //console.log(project);
+    handleSubmit(project);
+  };
+
+  function handleChange(event) {
+    setProject({ ...project, [event.target.name]: event.target.value });
+  }
+
+  function handleCategory(event) {
+    setProject({
+      ...project,
+      category: {
+        id: event.target.value,
+        name: event.target.options[event.target.selectedIndex].text,
+      },
+    });
+  }
 
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Nome do projeto:"
         name="name"
         placeholder="Insira o nome do projeto"
+        handlerOnChange={handleChange}
+        value={project.name ? project.name : ""}
       />
       <Input
         type="number"
         text="Orçamento do projeto:"
         name="budget"
         placeholder="Insira o orçamento total"
+        handlerOnChange={handleChange}
+        value={project.budget ? project.budget : ""}
       />
       <Select
         name="category_id"
         text="Selecione a categoria"
+        handlerOnChange={handleCategory}
         options={categories}
+        value={project.category ? project.category.id : ""}
       />
       <SubmitButton text={btnText} />
     </form>
